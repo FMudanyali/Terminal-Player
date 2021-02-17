ee2c03565adf0f227367164b711690745136600b
Furkan Mudanyalı <fmudanyali@icloud.com>
2021-02-17 19:50:29 +0300

    experimental color, multi-platform support

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
-[![](https://i.ytimg.com/vi/fFPdDQVyZrs/0.jpg)](https://www.youtube.com/watch?v=fFPdDQVyZrs)
+[![](https://i.ytimg.com/vi/m54ED2pGbQY/0.jpg)](https://youtu.be/m54ED2pGbQY)
 
 # Terminal Player
 A CLI video playback program in C, uses libavformat to read video data like framerate and duration, and uses ffmpeg to convert video to bmp. Yes, it is inefficient.
 
 ## Notes
-- Sound works only on WSL at the moment, would take minimal change to make it work on linux using aplay or something. I haven't tested this on anything else.
-- Aspect ratio may behave funnily if its anything other than 4:3, I took Bad Apple! as a reference while coding this, also line spacing plays a huge part on that, idk how im gonna
+- Works on WSL and macOS, haven't tried under linux but I think it should work just fine.
+- Resource heavy, the framerate may fluctuate or even entire thing can freeze if your computer is not powerful enough, I get full speeds with i7 2600k.
+- Planning to decode videos natively without depending on ffmpeg externally, also some threading or something.
+- Only WSL requires external audio playback, might use miniaudio or something on other systems.
+- WSL is also really hacky because of two reasons:
+- - powershell can't play the audio inside WSL directory because CMD cannot access network locations, yes, WSL path is in "\\$WSL", which is a network format. So it moves the audio to Windows temp folder first, then plays it back.
+- - I/O speed is crippled outside WSL directories, resulting in slow framerate, so the temporary bmp files are extracted in home folder instead of relative folder.
+- Since I am reading colors in 8bpp, and terminal has 8 colors only, it is hard to approximate colors, which means there are color artifacts.
+- Aspect ratio might be wrong if its anything other than 4:3, I took Bad Apple! as a reference while coding this, also line spacing plays a huge part on that, idk how im gonna
 approach to this.
 
+## Compilation instructions
+```
+mkdir build
+cd build
+cmake .. // add '-DWSL=1' without quotes if you want to compile for WSL.
+make
+```
+
+## Usage
+```
+./termplayer videofile.mp4 // plays in monochrome
+./termplayer videofile.mp4 ANY_COLOR // plays in monochrome but whites are in specified color
+./termplayer videofile.mp4 color // plays in 8 colors.
+```
 ## Dependencies
 - ffmpeg
 - libavformat
--- main.c
+++ main.c
@@ -1,19 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <ncurses.h>
 #include <libavformat/avformat.h>
 
 int max_y = 0, max_x = 0, curr_y = 0, curr_x = 0;
 
-void draw_bmp(int duration, int framerate){
+typedef struct {
+    unsigned short red:3;
+    unsigned short green:3;
+    unsigned short blue:2;
+} color8bpp;
+
+void get_color(unsigned char red, unsigned char green, unsigned char blue){
+    short has_red = 0, has_green = 0, has_blue = 0;
+    if(red > 3){
+        has_red = 4;
+    } if(green > 3){
+        has_green = 2;
+    } if(blue > 1){
+        has_blue = 1;
+    }
+    attron(COLOR_PAIR(has_red + has_green + has_blue));
+}
+
+void draw_bmp(int duration, int framerate, char *color){
     FILE *bmpHandle;
     int width, height, offset;
-    unsigned char color;
-    clock_t begin, end;
+    color8bpp pixel;
+    unsigned short brightness;
+    struct timeval tv;
+    unsigned long begin, end;
+    unsigned short use_colors = 0;
+    if(!strcmp(color, "color")) use_colors = 1;
+    else if(!strcmp(color, "blue")) attron(COLOR_PAIR(1));
+    else if(!strcmp(color, "green")) attron(COLOR_PAIR(2));
+    else if(!strcmp(color, "cyan")) attron(COLOR_PAIR(3));
+    else if(!strcmp(color, "red")) attron(COLOR_PAIR(4));
+    else if(!strcmp(color, "magenta")) attron(COLOR_PAIR(5));
+    else if(!strcmp(color, "yellow")) attron(COLOR_PAIR(6));
     for (int i = 1; i < duration * framerate; ++i){
-        begin = clock();
+        gettimeofday(&tv,NULL);
+        begin = 1000000 * tv.tv_sec + tv.tv_usec;
         getmaxyx(stdscr, max_y, max_x);
         if (max_y != curr_y || max_x != curr_x){
             curr_y = max_y;
@@ -21,7 +51,11 @@ void draw_bmp(int duration, int framerate){
             clear();
         }
         char buf[0x100];
+        #ifdef WSL
         snprintf(buf, sizeof(buf), "%s/temp-termplayer/bitmap%d.bmp", getenv("HOME"), i);
+        #else
+        snprintf(buf, sizeof(buf), "temp-termplayer/bitmap%d.bmp", i);
+        #endif
         bmpHandle = fopen(buf,"r");
         
         fseek(bmpHandle,0x0A,SEEK_SET);
@@ -32,17 +66,19 @@ void draw_bmp(int duration, int framerate){
 
         int true_width = max_y * 3;
         int jump_width = width/true_width;
-        int jump_height = height/max_y; 
+        int jump_height = height/max_y;
 
         offset += width * height - width;
         for(int j = 0; j < max_y; ++j){
             for(int k = 0; k < true_width; ++k){
                 fseek(bmpHandle,offset,SEEK_SET);
-                fread(&color,1,1,bmpHandle);
+                fread(&pixel,1,1,bmpHandle);
+                if(use_colors) get_color(pixel.red,pixel.green,pixel.blue);
+                brightness = (pixel.red<<5)|(pixel.green<<3)|pixel.blue;
                 move(j,k+(max_x-true_width)/2);
-                if (color > 176) addch('a');
-                else if (color > 128) addch(':');
-                else if (color > 0)  addch('.');
+                if (brightness > 148) addch('a');
+                else if (brightness > 96) addch(':');
+                else if (brightness > 0)  addch('.');
                 else addch(' ');
                 offset += jump_width;
             }
@@ -51,7 +87,8 @@ void draw_bmp(int duration, int framerate){
         }
         fclose(bmpHandle);
         refresh();
-        end = clock();
+        gettimeofday(&tv,NULL);
+        end = 1000000 * tv.tv_sec + tv.tv_usec;
         usleep(1000000/framerate - (end - begin));
     }
 }
@@ -62,7 +99,6 @@ int main(int argc, char *argv[]){
         return 0;
     }
 
-    av_register_all();
     AVFormatContext *pFormatCtx = NULL;
     if (avformat_open_input(&pFormatCtx, argv[1], NULL, NULL) != 0)
         return -1;
@@ -73,10 +109,17 @@ int main(int argc, char *argv[]){
     int duration = (int)(pFormatCtx->duration/1000000);
     int framerate = pFormatCtx->streams[0]->r_frame_rate.num / pFormatCtx->streams[0]->r_frame_rate.den;
     char buf[0x100];
+    #ifdef WSL
     system("mkdir ~/temp-termplayer/");
-    snprintf(buf, sizeof(buf), "ffmpeg -i %s -pix_fmt bgr8 -y ~/temp-termplayer/bitmap%%01d.bmp", argv[1]);
+    snprintf(buf, sizeof(buf), "ffmpeg -i \"%s\" -pix_fmt bgr8 -y ~/temp-termplayer/bitmap%%01d.bmp", argv[1]);
     system(buf);
-    snprintf(buf, sizeof(buf), "ffmpeg -i %s -y audio.wav", argv[1]);
+    snprintf(buf, sizeof(buf), "ffmpeg -i \"%s\" -y audio.wav", argv[1]);
+    #else
+    system("mkdir temp-termplayer/");
+    snprintf(buf, sizeof(buf), "ffmpeg -i \"%s\" -pix_fmt bgr8 -y temp-termplayer/bitmap%%01d.bmp", argv[1]);
+    system(buf);
+    snprintf(buf, sizeof(buf), "ffmpeg -i \"%s\" -y temp-termplayer/audio.wav", argv[1]);
+    #endif
     system(buf);
     initscr();
     noecho();
@@ -84,16 +127,39 @@ int main(int argc, char *argv[]){
     intrflush(stdscr, FALSE);
     curs_set(FALSE);
     nonl();
+    start_color();
+    init_pair(0, COLOR_BLACK, COLOR_BLACK);
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_CYAN, COLOR_BLACK);
+    init_pair(4, COLOR_RED, COLOR_BLACK);
+    init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(6, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(7, COLOR_WHITE, COLOR_BLACK);
     clear();
     refresh();
 
     int pid = fork();
     if (pid == 0){
-        system("powershell.exe -c \"mv audio.wav \\$env:temp\\audio.wav ; (New-Object Media.SoundPlayer \\$env:temp\\audio.wav).PlaySync();\" >/dev/null 2>/dev/null");
+        #ifdef WSL
+        system("powershell.exe -c \"del \\$env:temp\\audio.wav ; mv audio.wav \\$env:temp\\audio.wav ; (New-Object Media.SoundPlayer \\$env:temp\\audio.wav).PlaySync();\" >/dev/null 2>/dev/null");
+        #elif __linux
+        system("aplay temp-termplayer/audio.wav");
+        #else
+        system("afplay temp-termplayer/audio.wav");
+        #endif
+        return 0;
+    }
+    if (argc > 2){
+        draw_bmp(duration, framerate, argv[2]);
     } else {
-        draw_bmp(duration, framerate);
+        draw_bmp(duration, framerate, "monochrome");
     }
     endwin();
-    system("rm -r ~/temp-termplayer");
+    #ifdef WSL
+    system("rm -r ~/temp-termplayer/");
+    #else
+    system("rm -r temp-termplayer/");
+    #endif
 	return 0;
 }
